79b24606e6143ce7e24e21b48155727788fd0004
dnguye2 <dnguyen1093@gmail.com>
2020-03-29 22:43:28 -0700

    passed all tests for permutations

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,23 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+
+  return false if string1.length != string2.length
+
+  lookup_hash = {}
+  string1.each_char do |letter|
+    lookup_hash[letter] = 0
+  end
+
+  string2.each_char do |letter|
+    if lookup_hash[letter]
+      lookup_hash[letter] = true
+    end
+  end
+
+  puts lookup_hash
+  if lookup_hash.value?(0) 
+    return false
+  else
+    return true
+  end
+
 end
