50003edc4c2cb5ba94a1620e614db16602117f98
dnguye2 <dnguyen1093@gmail.com>
2020-03-29 23:20:27 -0700

    passed all tests

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,25 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  lookup_hash = {}
+  string.each_char do |letter|
+    lookup_hash[letter] = 0
+  end
+
+  string.each_char do |letter|
+    if lookup_hash[letter]
+      lookup_hash[letter] += 1
+    end
+  end
+
+  number_of_odd = 0
+  lookup_hash.each_value do |letter|
+    number_of_odd += 1 if letter.odd?
+  end
+  
+  if number_of_odd > 1
+      return false
+    else
+      return true
+  end
+
 end
