f40f45f28b5ea1b8bfd2875aaf360d9f93b10e3d
dnguye2 <dnguyen1093@gmail.com>
2020-03-29 22:06:45 -0700

    passed all test for array intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,26 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  # add to hash if not already in the hash
+  # setting up lookup hash
+  lookup_hash = {}
+  if list1.length < list2.length
+    list1.each do |num|
+      lookup_hash[num] = true
+    end
+
+    intersection_array = list2.map do |num|
+      num if lookup_hash[num]
+    end
+
+    return intersection_array.compact
+  else
+    list2.each do |num|
+      lookup_hash[num] = true
+    end
+
+    intersection_array = list1.map do |num|
+      num if lookup_hash[num]
+    end
+
+    return intersection_array.compact
+  end
 end
